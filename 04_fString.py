c6cf2e28909f849228f17bc3928149ff524dfb68
Samson Oladimeji Odunaiya <102425294+Samklugh@users.noreply.github.com>
2025-12-29 15:08:49 +0000

    Delete 04_fString.py

--- 04_fString.py
+++
@@ -1,8 +0,0 @@
-# the f-string means formatted string literals. 
-
-#its a way to embed expressions inside string literals, using curly braces {}.
-
-name = "Alice"
-age = 25
-
-print(f"My name is {name} and I am {age} years old.")
