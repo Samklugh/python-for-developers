e0c491dfc06066a46eb39d2c4883d909f2d76342
Samson Oladimeji Odunaiya <102425294+Samklugh@users.noreply.github.com>
2025-12-29 15:09:02 +0000

    Delete 05_input.py

--- 05_input.py
+++
@@ -1,5 +0,0 @@
-location= input("Enter your location: ")
-vacation_place= input("Enter your dream vacation place: ")
-
-print(f"You are located in {location}.")
-print(f"Your dream vacation place is {vacation_place}.")
